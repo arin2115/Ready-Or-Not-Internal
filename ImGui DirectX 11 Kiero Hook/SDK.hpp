18a731bdf1b0aae0b4b86c929d587f3a7e1b810f
arin2115 <ceo@itsarin.pl>
2025-10-21 00:07:51 +0200

    sd

---
+++ ImGui DirectX 11 Kiero Hook/SDK.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "UnrealContainers.hpp"
+#include "PropertyFixup.hpp"
+
+// Core Unreal Engine types and classes
+#include "SDK/Basic.hpp"
+#include "SDK/CoreUObject_structs.hpp"
+#include "SDK/CoreUObject_classes.hpp"
+#include "SDK/Engine_structs.hpp"
+#include "SDK/Engine_classes.hpp"
+#include "SDK/InputCore_classes.hpp"
+
+// Ready Or Not game classes
+#include "SDK/ReadyOrNot_structs.hpp"
+#include "SDK/ReadyOrNot_classes.hpp"
+
+// Player and controller classes
+#include "SDK/BasePlayer_classes.hpp"
+#include "SDK/BP_BaseController_classes.hpp"
